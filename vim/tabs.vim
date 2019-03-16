61519ceb1f111cb64271d87ca624a88ebb8961c2
mlrv <marco.levrero@gmail.com>
2019-03-16 20:58:45 +0000

    Initial commit

---
+++ vim/tabs.vim
@@ -0,0 +1,4 @@
+" For language specific tab settings
+
+autocmd FileType js set tabstop=4 softtabstop=0 expandtab shiftwidth=2 smarttab
+autocmd FileType html set tabstop=4 softtabstop=0 expandtab shiftwidth=2 smarttab
---
+++ zsh/zshrc.sh
@@ -0,0 +1,71 @@
+# Vars
+	HISTFILE=~/.zsh_history
+	SAVEHIST=1000 
+	setopt inc_append_history # To save every command before it is executed 
+	setopt share_history # setopt inc_append_history
+
+	git config --global push.default current
+
+# Aliases
+	alias c="clear"
+	mkdir -p /tmp/log
+	
+# Settings
+	export VISUAL=vim
+
+source ~/dotfiles/zsh/plugins/fixls.zsh
+
+#Functions
+	# Loop a command and show the output in vim
+	loop() {
+		echo ":cq to quit\n" > /tmp/log/output 
+		fc -ln -1 > /tmp/log/program
+		while true; do
+			cat /tmp/log/program >> /tmp/log/output ;
+			$(cat /tmp/log/program) |& tee -a /tmp/log/output ;
+			echo '\n' >> /tmp/log/output
+			vim + /tmp/log/output || break;
+			rm -rf /tmp/log/output
+		done;
+	}
+
+# For vim mappings: 
+	stty -ixon
+
+# Completions
+autoload -U compinit
+
+plugins=(
+	docker
+)
+
+for plugin ($plugins); do
+    fpath=(~/dotfiles/zsh/plugins/oh-my-zsh/plugins/$plugin $fpath)
+done
+
+compinit
+
+source ~/dotfiles/zsh/plugins/oh-my-zsh/lib/history.zsh
+source ~/dotfiles/zsh/plugins/oh-my-zsh/lib/key-bindings.zsh
+source ~/dotfiles/zsh/plugins/oh-my-zsh/lib/completion.zsh
+source ~/dotfiles/zsh/plugins/vi-mode.plugin.zsh
+source ~/dotfiles/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ~/dotfiles/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source ~/dotfiles/zsh/keybindings.sh
+
+# Fix for arrow-key searching
+# start typing + [Up-Arrow] - fuzzy find history forward
+if [[ "${terminfo[kcuu1]}" != "" ]]; then
+	autoload -U up-line-or-beginning-search
+	zle -N up-line-or-beginning-search
+	bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search
+fi
+# start typing + [Down-Arrow] - fuzzy find history backward
+if [[ "${terminfo[kcud1]}" != "" ]]; then
+	autoload -U down-line-or-beginning-search
+	zle -N down-line-or-beginning-search
+	bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
+fi
+
+source ~/dotfiles/zsh/prompt.sh
+export PATH=$PATH:$HOME/dotfiles/utils
