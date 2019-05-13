aa1e3adab24067ec09c76ce39b7893460bbf9944
Marco Levrero <marco@Marcos-MacBook-Pro.local>
2019-05-13 15:59:23 +0100

    Make bindings work with macOS

--- zsh/keybindings.sh
+++ zsh/keybindings.sh
@@ -4,7 +4,7 @@
 		zle accept-line
 	}
 	zle -N up_widget
-	bindkey "^k" up_widget
+	bindkey "^[k" up_widget
 
 # home
 	function goto_home() { 
@@ -13,7 +13,7 @@
 		zle accept-line
 	}
 	zle -N goto_home
-	bindkey "^h" goto_home
+	bindkey "^[h" goto_home
 
 # LS
 	function ctrl_l() {
@@ -21,7 +21,7 @@
 		zle accept-line
 	}
 	zle -N ctrl_l
-	bindkey "^l" ctrl_l
+	bindkey "^[l" ctrl_l
 
 # Sudo
 	function add_sudo() {
@@ -29,20 +29,4 @@
 		zle end-of-line
 	}
 	zle -N add_sudo
-	bindkey "^s" add_sudo
-
-# vim
-	function open_vim() {
-		BUFFER="vim"
-		zle accept-line
-	}
-	zle -N open_vim
-	bindkey "^v" open_vim
-
-	# chrome
-	function open_chrome() {
-		BUFFER="google-chrome"
-		zle accept-line
-	}
-	zle -N open_chrome
-	bindkey "^z" open_chrome
+	bindkey "^[s" add_sudo
